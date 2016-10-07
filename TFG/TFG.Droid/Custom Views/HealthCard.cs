9ec832e505e9dbb4016bd08ad41c841041ba367a
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-07 11:09:51 +0200

    Implementation for HealthCard

--- TFG/TFG.Droid/Custom Views/HealthCard.cs
+++ TFG/TFG.Droid/Custom Views/HealthCard.cs
@@ -9,8 +9,40 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
 
 namespace TFG.Droid.Custom_Views {
-    class HealthCard {
+    class HealthCard : LinearLayout{
+
+        public  TextView _moduleName;
+        private ImageView _moduleImage;
+
+        public string Name {
+            set { _moduleName.Text = value; }
+            get { return _moduleName.Text;  }
+        }
+
+
+        public HealthCard(Context context) : base(context) {
+            Init();
+        } 
+        public HealthCard(Context context, IAttributeSet attrs): base(context, attrs) {
+            Init();
+        } 
+
+
+        private void Init() {
+            var inflater = LayoutInflater.From(Context);
+            inflater.Inflate(Resource.Layout.health_card, this);
+
+            _moduleName = FindViewById<TextView>(Resource.Id.module_name);
+            _moduleImage = FindViewById<ImageView>(Resource.Id.module_image);
+
+        }
+         
+         
+
+
+
     }
 }
