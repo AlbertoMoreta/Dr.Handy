f41f2e91e3548f25c41ba856a6ff6200bf6be310
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-16 15:13:28 +0200

    Added shared classes for Model and Logic of Color Blindness Test

---
+++ TFG/TFG/Logic/ColorBlindnessLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TFG.Logic {
+    class ColorBlindnessLogic {
+
+        public List<Model.ColorBlindnessQuestion> Questions { get; set; }
+
+#if __ANDROID__
+        private Android.Content.Context _context;
+
+        public ColorBlindnessLogic(Android.Content.Context context){
+            _context = context;
+        }
+
+#endif
+
+
+        public List<Model.ColorBlindnessQuestion> GetQuestions()  {
+
+            if (Questions == null) {
+                string jsonFile;
+                string fileName = "JSON/ColorBlindnessTest.json";
+#if __IOS__
+            jsonFile = File.ReadAllText(fileName);
+#elif __ANDROID__
+                StreamReader stream = new StreamReader(_context.Assets.Open(fileName));
+                jsonFile = stream.ReadToEnd();
+                stream.Close();
+
+#endif
+                Questions = JsonConvert.DeserializeObject<List<Model.ColorBlindnessQuestion>>(jsonFile);
+            }
+
+            return Questions;;
+        }
+
+    }
+}
---
+++ TFG/TFG/Model/ColorBlindnessQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TFG.Model {
+    class ColorBlindnessQuestion {
+
+        public string ImageName { get; set; }
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+        public string CorrectAnswer { get; set; }
+          
+    
+    }
+}
