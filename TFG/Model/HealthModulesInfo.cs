e9e29eb5b8e5512f0cba29a8183cc7b3320615b8
Alberto Moreta <albertomoreta70@gmail.com>
2016-11-15 20:02:55 +0100

    Added Step Counter module info

--- TFG/Model/HealthModulesInfo.cs
+++ TFG/Model/HealthModulesInfo.cs
@@ -5,21 +5,21 @@ using System.Text;
 namespace TFG.Model {
 
     public enum HealthModuleType {
-        ColorBlindnessTest, Module2, Module3
+        ColorBlindnessTest, StepCounter, Module3
     }
 
     public static class HealthModulesInfo {
 
         public static List<HealthModuleType> GetHealthModules { get; private set; } = new List<HealthModuleType> {
             HealthModuleType.ColorBlindnessTest,
-            HealthModuleType.Module2,
+            HealthModuleType.StepCounter,
             HealthModuleType.Module3
         };
 
         public static string HealthModuleName(this HealthModuleType module) {
             switch (module) {
                 case HealthModuleType.ColorBlindnessTest: return GetStringFromResourceName("color_blindness_test_name");
-                case HealthModuleType.Module2: return "Module2";
+                case HealthModuleType.StepCounter: return GetStringFromResourceName("step_counter_name");
                 case HealthModuleType.Module3: return "Module3";
                 default: return "Error";
             }
@@ -29,7 +29,7 @@ namespace TFG.Model {
         public static string HealthModuleDescription (this HealthModuleType module) {
             switch (module) {
                 case HealthModuleType.ColorBlindnessTest: return "Module1 Description";
-                case HealthModuleType.Module2: return "Module2 Description";
+                case HealthModuleType.StepCounter: return "Module2 Description";
                 case HealthModuleType.Module3: return "Module3 Description";
                 default: return "Error";
             }
