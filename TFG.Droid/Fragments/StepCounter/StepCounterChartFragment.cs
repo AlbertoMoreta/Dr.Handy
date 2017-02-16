6064aa01688dc9b101584ed1e2213567466b1fea
Alberto Moreta <albertomoreta70@gmail.com>
2017-02-16 12:49:09 +0100

    Set Color for StepCounter charts

--- TFG.Droid/Fragments/StepCounter/StepCounterChartFragment.cs
+++ TFG.Droid/Fragments/StepCounter/StepCounterChartFragment.cs
@@ -7,15 +7,18 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.Content;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Java.Lang;
 using MikePhil.Charting.Charts;
 using MikePhil.Charting.Components;
+using TFG.Droid.Activities;
 using TFG.Droid.Custom_Views;
 using TFG.Droid.Interfaces;
-using TFG.Droid.Utils; 
+using TFG.Droid.Utils;
+using TFG.Model;
 using Fragment = Android.Support.V4.App.Fragment;
 
 namespace TFG.Droid.Fragments.StepCounter {
@@ -36,17 +39,25 @@ namespace TFG.Droid.Fragments.StepCounter {
             var view =  inflater.Inflate(Resource.Layout.fragment_stepcounter_body_chart_results, container, false);
 
             var itemsForMetric = ChartUtils.GetStepCounterItemsFromMetric(_metric, DateTime.Now);
+            var colorName =
+                HealthModulesInfo.GetHealthModuleColorFromHealthModuleName(
+                    ((ModuleDetailActivity) Activity).CurrentHealthModule);
+            var colorRes = ContextCompat.GetColor(Activity, Activity.Resources.GetIdentifier(colorName, "color", Activity.PackageName));
+
 
             var stepsChart = view.FindViewById<CardViewBarChart>(Resource.Id.steps_chart);
             stepsChart.Title.Text = Activity.GetString(Activity.Resources.GetIdentifier("steps", "string", Activity.PackageName));
+            stepsChart.Color = colorRes;
             stepsChart.PopulateChart(ChartUtils.StepCounter_StepsToBarEntries(itemsForMetric));
 
             var caloriesChart = view.FindViewById<CardViewBarChart>(Resource.Id.calories_chart);
             caloriesChart.Title.Text = Activity.GetString(Activity.Resources.GetIdentifier("calories", "string", Activity.PackageName));
+            caloriesChart.Color = colorRes;
             caloriesChart.PopulateChart(ChartUtils.StepCounter_CaloriesToBarEntries(itemsForMetric));
 
             var distanceChart = view.FindViewById<CardViewBarChart>(Resource.Id.distance_chart);
             distanceChart.Title.Text = Activity.GetString(Activity.Resources.GetIdentifier("kilometers", "string", Activity.PackageName));
+            distanceChart.Color = colorRes;
             distanceChart.PopulateChart(ChartUtils.StepCounter_DistanceToBarEntries(itemsForMetric));
 
             return view;
