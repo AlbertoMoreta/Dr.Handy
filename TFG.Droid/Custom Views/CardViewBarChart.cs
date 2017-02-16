297312f23962c37cf5b4d0a0f647f4474c8bdfaa
Alberto Moreta <albertomoreta70@gmail.com>
2017-02-16 11:08:03 +0100

    Created custom view with CardView and BarChart

---
+++ TFG.Droid/Custom Views/CardViewBarChart.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MikePhil.Charting.Charts;
+using MikePhil.Charting.Components;
+using MikePhil.Charting.Data;
+using TFG.Droid.Utils;
+
+namespace TFG.Droid.Custom_Views {
+    class CardViewBarChart : CardView{
+
+        public BarChart Chart { get; set; }
+
+        public CardViewBarChart(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { Init(); }
+        public CardViewBarChart(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { Init(); }
+        public CardViewBarChart(Context context, IAttributeSet attrs) : base(context, attrs) { Init(); }
+        public CardViewBarChart(Context context) : base(context) { Init(); }
+
+        private void Init() {
+            var inflater = LayoutInflater.From(Context);
+            inflater.Inflate(Resource.Layout.cardview_barchart, this);
+
+            Chart = FindViewById<BarChart>(Resource.Id.chart);
+            Chart.SetDrawBarShadow(true);
+            Chart.SetDrawGridBackground(false);
+            Chart.Legend.Enabled = false;
+            Chart.SetDrawValueAboveBar(true);
+            Chart.HighlightPerDragEnabled = false;
+            Chart.HighlightPerTapEnabled = false;
+            Chart.ScaleYEnabled = false;
+            Chart.AnimateY(700);
+            Chart.ScaleX = 1;
+            Chart.Description.Text = "";
+
+            //X Axis Properties
+            var xAxis = Chart.XAxis;
+            xAxis.Position = XAxis.XAxisPosition.Bottom;
+            xAxis.SetDrawGridLines(false);
+
+            //Left Axis Properties
+            var leftAxis = Chart.AxisLeft;
+            leftAxis.AxisMaximum = 100f;
+            leftAxis.AxisMinimum = 0f;
+            leftAxis.SetDrawGridLines(false);
+            leftAxis.SetLabelCount(5, true);
+
+            //Disable Right Axis
+            var rightAxis = Chart.AxisRight;
+            rightAxis.Enabled = false; 
+        }
+
+        public void PopulateChart(List<BarEntry> barEntries) {
+            if (Chart != null) {
+                Chart.Data = new BarData(new BarDataSet(barEntries, ""));
+                //Chart.AnimateY(700);
+                Chart.NotifyDataSetChanged();
+                Chart.Invalidate();
+            }
+        }
+    }
+}
