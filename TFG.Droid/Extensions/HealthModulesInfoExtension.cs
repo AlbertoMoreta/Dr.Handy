3e8e85eb1fcb7d8ad19192159bfcf3c3966d6f0b
Alberto Moreta <albertomoreta70@gmail.com>
2017-02-18 10:45:07 +0100

    Use this branch for the development of the android step counter

--- TFG.Droid/Extensions/HealthModulesInfoExtension.cs
+++ TFG.Droid/Extensions/HealthModulesInfoExtension.cs
@@ -9,7 +9,6 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using TFG.Droid.Fragments.ColorBlindnessTest;
 using TFG.Droid.Fragments.StepCounter;
 using TFG.Droid.Interfaces;
 using TFG.Model;
@@ -18,16 +17,12 @@ namespace TFG.Droid{
     public static class HealthModulesInfoExtension {
 
         public static IHealthFragment GetHeaderFragmentFromHealthModuleName(string moduleName) {
-            if (HealthModuleType.ColorBlindnessTest.HealthModuleName().Equals(moduleName)) { return new CBTHeaderFragment(); }
             if (HealthModuleType.StepCounter.HealthModuleName().Equals(moduleName)) { return new StepCounterHeaderFragment(); }
-            if (HealthModuleType.Module3.HealthModuleName().Equals(moduleName)) { return null; }
             return null;
         }
 
         public static IHealthFragment GetBodyFragmentFromHealthModuleName(string moduleName) {
-            if (HealthModuleType.ColorBlindnessTest.HealthModuleName().Equals(moduleName)) { return new CBTBodyFragment(); } 
             if (HealthModuleType.StepCounter.HealthModuleName().Equals(moduleName)) { return new StepCounterBodyFragment(); } 
-            if (HealthModuleType.Module3.HealthModuleName().Equals(moduleName)) { return null; }
             return null;
         }
 
