7a412b9883de83b09849eced78d0b2b651c3bd90
Alberto Moreta <albertomoreta70@gmail.com>
2017-03-06 18:54:10 +0100

    Fixed previous commit

--- TFG/Logic/SintromLogic.cs
+++ TFG/Logic/SintromLogic.cs
@@ -18,8 +18,12 @@ namespace TFG.Logic {
         private SintromLogic() { }
 
         public NotificationItem GetNotificationitem() {
-           var sintromItem = DBHelper.Instance.GetSintromItemFromDate(DateTime.Now);
-            sintromItem.
+            var sintromItem = DBHelper.Instance.GetSintromItemFromDate(DateTime.Now); 
+
+            var title = HealthModulesInfo.GetStringFromResourceName("sintrom_name");
+            var description = String.format(HealthModulesInfo.GetStringFromResourceName("sintrom_notification_description"), sintromItem.Fraction, Medicine);
+
+            return new NotificationItem(title, description, true);
         }
 
     }
