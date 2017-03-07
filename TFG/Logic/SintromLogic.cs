b426019f06aed516c82697d1996aedf6d2dfd01f
Alberto Moreta <albertomoreta70@gmail.com>
2017-03-07 14:26:32 +0100

    Set notification for sintrom control days

--- TFG/Logic/SintromLogic.cs
+++ TFG/Logic/SintromLogic.cs
@@ -19,17 +19,28 @@ namespace TFG.Logic {
         private SintromLogic() { }
 
         public NotificationItem GetNotificationitem() {
+            var controlday = DBHelper.Instance.GetSintromINRItemFromDate(DateTime.Now);
+            var title = HealthModulesInfo.GetStringFromResourceName("sintrom_name");
+            //Control Day Notification
+            if (controlday.Count > 0 && controlday[0].Control) {
+                var description =
+                    string.Format(HealthModulesInfo.GetStringFromResourceName("sintrom_notification_control_description"));
+
+                return new NotificationItem(title, description, true);
+            }
+
+            //Treatment Day Notification
             var sintromItems = DBHelper.Instance.GetSintromItemFromDate(DateTime.Now);
             if (sintromItems.Count > 0) {
                 var sintromItem = sintromItems[0];
-                var title = HealthModulesInfo.GetStringFromResourceName("sintrom_name");
                 var description =
                     string.Format(HealthModulesInfo.GetStringFromResourceName("sintrom_notification_description"),
                         sintromItem.Fraction, sintromItem.Medicine);
 
                 return new NotificationItem(title, description, true);
-            }
+            } 
 
+            //No Notification
             return null;
         }
 
