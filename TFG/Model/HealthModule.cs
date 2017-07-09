f01caa4da4f46ed0da15c689b94a9c2023c78130
Alberto Moreta <albertomoreta70@gmail.com>
2017-07-09 11:12:28 +0200

    Get the module configuration properties from a JSON file instead of HealthModulesInfo

--- TFG/Model/HealthModule.cs
+++ TFG/Model/HealthModule.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using TFG.Logic;
+
+#if __ANDROID__
+using Android.Graphics.Drawables;
+using Android.Content;
+using Android.Content.Res;
+using Android.Support.V4.Content;
+#endif
 
 namespace TFG.Model {
-    public class HealthModule  {
+    public partial class HealthModule  {
 
         public int Id { get; set; }
+
+        [JsonProperty(Required = Newtonsoft.Json.Required.Always)]
+        [JsonConverter(typeof(CurrentLanguageConverter))]
         public string Name { get; set; }
+
+        [JsonProperty(Required = Newtonsoft.Json.Required.Always)]
+        [JsonConverter(typeof(CurrentLanguageConverter))]
         public string Description { get; set; }
+
+        [JsonProperty(Required = Newtonsoft.Json.Required.DisallowNull)]
+        public string Color { get; set; } = "red";
+
+        [JsonProperty(PropertyName = "utils-class", Required = Newtonsoft.Json.Required.Always)] 
+        public string UtilsClass { get; set; }
+
+
+        public string ConfigFilePath { get; set; }
         public int Position { get; set; }
-        public bool Visible { get; set; } 
+        public bool Visible { get; set; }
+ 
+
     }
 }
