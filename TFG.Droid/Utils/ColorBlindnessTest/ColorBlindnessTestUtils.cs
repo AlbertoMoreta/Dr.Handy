da32fe9cd02090c710b49f3c5bf8a4167d895439
Alberto Moreta <albertomoreta70@gmail.com>
2017-07-10 16:03:12 +0200

    Added ColorBlindnessTest Uitls class and config file

---
+++ TFG.Droid/Utils/ColorBlindnessTest/ColorBlindnessTestUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TFG.Droid.Fragments.ColorBlindnessTest;
+using TFG.Droid.Interfaces;
+
+namespace TFG.Droid.Utils {
+    class ColorBlindnessTestUtils : HealthModuleUtils  {
+        public override void InitModuleDB() { }
+
+        public override Drawable GetHealthModuleIcon(Context context) {
+            return GetDrawableFromResources(context, "colorblindnesstest_icon");
+        }
+
+        public override IHealthFragment GetHeaderFragment() {
+            return  new CBTHeaderFragment();
+        }
+
+        public override IHealthFragment GetBodyFragment() {
+            return new CBTBodyFragment();
+        }
+
+        public override IHealthFragment GetHealthCardFragment(string name) {
+            return null;
+        }
+    }
+}
